485befb56e2e1a00c6b2ebae9d785398f0a75af6
Tatsiana <timoha-tatiana@mail.ru>
2022-11-30 21:58:47 +0300

    corrected

--- Homework_4/exercise_2.py
+++ Homework_4/exercise_2.py
@@ -3,22 +3,25 @@
 # Удалите второй элемент. 
 # Добавьте в конец ключ «new_key» со значением «new_value».
 # Выведите на печать итоговый словарь. 
+from collections import OrderedDict
 
-
-book_of_phone_number_list = [
+book_numbers = [
     ['Tatsiana', '+375 25 222 22 22'], 
     ['Artem', '+375 29 888 88 88'], 
     ['Alex', '+375 44 555 55 55'], 
     ['Galina', '+375 29 999 99 99'], 
     ['Kirill', '+375 29 777 77 77'],
 ]
-book_numbers = dict(book_of_phone_number_list)
+
+ordered_dict = OrderedDict(book_numbers)
+print (ordered_dict)
 
 # Меняем местами первый и последний элементы
+ordered_dict.move_to_end('Tatsiana')
+ordered_dict.move_to_end('Kirill', False)
+print(ordered_dict)
 
-# Удаляем второй элемент
-del book_numbers ['Alex']
-
-# Вставляем новый элемент
-book_numbers['Anton'] = '+375 44 222 22 22'
-print(book_numbers)
+# Удаляем второй элемент и вставляем новый
+ordered_dict.pop('Alex')
+ordered_dict['Anton'] = '+375 44 222 22 22'
+print(ordered_dict)
