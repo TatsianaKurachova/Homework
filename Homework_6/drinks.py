59b386a40d1449e31a8fec6feec9d29cb04982de
Tatsiana <timoha-tatiana@mail.ru>
2022-12-15 21:43:56 +0300

    new_1

--- Homework_6/drinks.py
+++ Homework_6/drinks.py
@@ -3,10 +3,10 @@ import requests
 def response():
     r = requests.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
     data = r.json()
-    str = ''
+    dict = ''
     for item in data['drinks']:
-        str += (f"Cocktail_Name: {item['strDrink']}, How_to_do: {item['strInstructions']}, Picture: {item['strDrinkThumb']} \n")
-    return str
+        dict += (f"Name: {item['strDrink']} \n, How_to_do: {item['strInstructions']} \n, Picture: {item['strDrinkThumb']} \n")
+    return dict
  
 def record(x):
     with open('cocktails.txt', 'w+') as f:
