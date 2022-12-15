66918feef125e881ee310f40546b9055fdc355f6
Tatsiana <timoha-tatiana@mail.ru>
2022-12-15 18:40:49 +0300

    'new_homework_drinks

--- Homework_5/exercise_5.py
+++ Homework_5/exercise_5.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from datetime import timedelta
-start = timedelta (hours=9, minutes=40)
-finish = timedelta (hours=12, minutes=50)
-way = finish - start 
+from datetime import datetime, timedelta
+
+start = timedelta (hours=12, minutes=10)
+finish =timedelta (hours=14, minutes=40)
+way = finish - start
 print (way)
